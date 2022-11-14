4a848af325cdc7e7d86944f9dd0c027d6f8d2e2e
sGolo <sgolovic1@gmail.com>
2022-11-14 23:10:07 +0100

    unfinished

--- app.js
+++ app.js
@@ -1,9 +1,27 @@
+const inputAuthor = document.getElementById('inputAuthor');
+const inputTitle = document.getElementById('inputTitle');
+const inputPages = document.getElementById('inputPages');
+const submitBtn = document.getElementById('submitBtn');
+
+let bookAuthor = inputAuthor.value;
+let bookTitle = inputTitle.value;
+let bookPages = inputPages.value;
+
 let mylibrary = [];
 
-const Book() {
-
+function Book(author, title, pages) {
+  this.author = author;
+  this.title = title;
+  this.pages = pages;
 }
 
-
-const addBookToLibrary() {
+function addBookToLibrary() {
+  let bookAuthor = inputAuthor.value;
+  let bookTitle = inputTitle.value;
+  let bookPages = inputPages.value;
+  console.log(bookAuthor, bookTitle, bookPages);
+  const newBook = new Book();
 }
+submitBtn.addEventListener('click', (e) => {
+  console.log(bookAuthor, bookTitle, bookPages);
+});
